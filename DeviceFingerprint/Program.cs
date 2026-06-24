4ebd845cce60acf7c06add3b746e4ee1ac7753c2
kashi93 <75873210+kashi93@users.noreply.github.com>
2026-06-24 16:09:30 +0800

    initial commit

---
+++ DeviceFingerprint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeviceFingerprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
